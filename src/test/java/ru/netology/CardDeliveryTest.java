49127ee3d46197624ed50d9fd6e7187c7a07c8fe
vikdm <viktorov@my.com>
2022-07-30 16:34:25 +0300

    Тест

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryTest {
+
+    @BeforeEach
+    void openWebBank() {
+        open("http://localhost:7777/");
+
+    }
+
+    @Test
+    void testDelivery() {
+        Configuration.timeout = 15;
+
+        $("[data-test-id=city] input").val("Москва");
+        $x("//input[@type='tel']").pressTab();
+        $x("//input[@name='name']").val("Иван Петров-Водкин");
+        $x("//input[@name='phone']").val("+79001231212");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+}
