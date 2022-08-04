69721fb905a9029a8fb725891ebece64daec6f7f
vikdm <viktorov@my.com>
2022-08-04 21:41:41 +0300

    CardDeliveryTest v. 7

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -34,7 +34,7 @@ class CardDeliveryTest {
         $x("//input[@name='phone']").val("+79991112233");
         $("[data-test-id=agreement]").click();
         $(".button.button_view_extra.button_size_m").click();
-        $("[data-test-id='success-notification']")
+        $("[data-test-id=success-notification]")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + planDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
 
